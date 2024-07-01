f053a18eca21055d9c62ba8dff8a670b481bc0fb
Infant Ajay Venus <infantcsajay@outlook.com>
2024-07-01 23:13:09 +0530

    [body] patch: animate title

--- src/Todo/layout/MainLayout/MainSection/Body.tsx
+++ src/Todo/layout/MainLayout/MainSection/Body.tsx
@@ -1,3 +1,4 @@
+import {motion} from 'framer-motion';
 import { LayoutProps } from "..";
 
 export interface MainSectionBodyProps extends LayoutProps {
@@ -9,7 +10,20 @@ export function Body({ title, children }: MainSectionBodyProps) {
     return (
         <section className='flex flex-col min-h-[90%] lg:min-h-[95%] lg:pl-[20%]'>
             <header className='pt-2 pb-4'>
-                <h3 className='text-2xl font-bold'>{title}</h3>
+                <motion.h3 
+                    className='text-2xl font-bold'
+                    initial={{
+                        opacity: 0,
+                        x: -20
+                    }}
+                    animate={{
+                        opacity: 1,
+                        x: 0,
+                    }}
+                    transition={{
+                        duration: 0.2,
+                    }}
+                >{title}</motion.h3>
             </header>
             {children}
         </section>
