8116a5b0d9e2e3188c19b9e6143e9bb71f9815a1
Infant Ajay Venus <infantcsajay@outlook.com>
2024-07-03 00:23:59 +0530

    [Body] refactory: update body to be more generic

--- src/Todo/layout/MainLayout/MainSection/Body.tsx
+++ src/Todo/layout/MainLayout/MainSection/Body.tsx
@@ -1,31 +1,20 @@
-import {motion} from 'framer-motion';
 import { LayoutProps } from "..";
 
-export interface MainSectionBodyProps extends LayoutProps {
-    title: string,
-};
+export interface MainSectionBodyProps extends LayoutProps {};
+
+export interface BodyHeaderProps extends LayoutProps {};
 
 
-export function Body({ title, children }: MainSectionBodyProps) {
+export function Body({ children }: MainSectionBodyProps) {
     return (
         <section className='flex flex-col min-h-[90%] lg:min-h-[95%] lg:pl-[20%]'>
-            <header className='pt-2 pb-4'>
-                <motion.h3 
-                    className='text-2xl font-bold'
-                    initial={{
-                        opacity: 0,
-                        x: -20
-                    }}
-                    animate={{
-                        opacity: 1,
-                        x: 0,
-                    }}
-                    transition={{
-                        duration: 0.2,
-                    }}
-                >{title}</motion.h3>
-            </header>
             {children}
         </section>
     )
 }
+
+Body.Header = function Header({children}: BodyHeaderProps) {
+    return (
+        <header className='pt-2 pb-4'>{children}</header>
+    )
+};
