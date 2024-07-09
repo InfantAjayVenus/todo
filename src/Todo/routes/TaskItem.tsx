80a39d2467489856a86e4b19edef4ddd0c9fbc34
Infant Ajay Venus <infantcsajay@outlook.com>
2024-07-09 16:35:51 +0530

    [taskitem] feat: add go back navigation

--- src/Todo/routes/TaskItem.tsx
+++ src/Todo/routes/TaskItem.tsx
@@ -1,14 +1,21 @@
-import { useParams } from "react-router-dom";
-import { MainSection } from "../components/MainLayout";
+import { Button } from "@/components/ui/button";
+import { IoClose } from "react-icons/io5";
+import { useNavigate, useParams } from "react-router-dom";
 import { Container } from "../components/Container";
+import { MainSection } from "../components/MainLayout";
 
 export function TaskItem() {
-    const {taskId} = useParams();
+    const { taskId } = useParams();
+    const navigate = useNavigate();
+    
     return (
         <>
-            <MainSection.Body.Title>
-                {taskId}
-            </MainSection.Body.Title>
+            <MainSection.Body.Header className="flex items-center justify-between text-center">
+                <MainSection.Body.Title>
+                    {taskId}
+                </MainSection.Body.Title>
+                <Button variant={'ghost'} className="text-3xl" onClick={() => navigate(-1)}><IoClose /> </Button>
+            </MainSection.Body.Header>
             <Container>body</Container>
         </>
     )
