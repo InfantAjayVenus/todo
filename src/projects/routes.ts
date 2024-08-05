bade6dfb1144f5d8d61e66fab128c41c5218add8
Infant Ajay Venus <infantcsajay@outlook.com>
2024-08-05 16:21:50 +0530

    [project] fix: add error handling and status return to routes

--- src/projects/routes.ts
+++ src/projects/routes.ts
@@ -4,6 +4,13 @@ import { getProjects } from './controller';
 export const projectRouter = express.Router();
 projectRouter.use(express.json());
 
-projectRouter.get('/', (req, res) => {
-    res.json(getProjects('91823749'))
+projectRouter.get('/', async (req, res) => {
+    try {
+        const filteredProjects = await getProjects('91823749');
+        res.status(200).json(filteredProjects);
+    } catch (error) {
+        res.status(500).json({
+            error: "Unable to process request. Please try again after a while :("
+        })
+    }
 });
