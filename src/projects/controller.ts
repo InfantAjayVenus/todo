de129ea87d17b43fc00168f5f85cf39520e20a76
Infant Ajay Venus <infantcsajay@outlook.com>
2024-08-11 10:42:16 +0530

    [project] feat: implement controller for get project by Id

--- src/projects/controller.ts
+++ src/projects/controller.ts
@@ -7,15 +7,15 @@ import { writeFileSync } from 'fs';
 const dbPath = path.join(__dirname, 'db.json');
 
 export async function getProjects(creatorId: string) {
-    return projectDB.filter(({creator_id}) => creatorId === creator_id);
+    return projectDB.filter(({ creator_id }) => creatorId === creator_id);
 }
 
-export async function addProject(projectData:Pick<Project, 'name'|'creator_id' | 'order' | 'color' | 'parent_id'>): Promise<Project> {
+export async function addProject(projectData: Pick<Project, 'name' | 'creator_id' | 'order' | 'color' | 'parent_id'>): Promise<Project> {
 
-    if(projectData?.name.length > 0) throw("Name is a required field");
+    if (projectData?.name.length > 0) throw ("Name is a required field");
 
-    const userProjects = projectDB.filter(({creator_id}) => creator_id === projectData.creator_id);
-    if(userProjects.some(({name}) => name === projectData.name)) throw("A Project with the name already exists");
+    const userProjects = projectDB.filter(({ creator_id }) => creator_id === projectData.creator_id);
+    if (userProjects.some(({ name }) => name === projectData.name)) throw ("A Project with the name already exists");
 
     const newProject: Project = {
         id: randomUUID(),
@@ -32,4 +32,16 @@ export async function addProject(projectData:Pick<Project, 'name'|'creator_id' |
     projectDB.push(newProject);
     writeFileSync(dbPath, JSON.stringify(projectDB, null, 2));
     return newProject;
+}
+
+export async function getProjectById(creatorId: string, projectId: string): Promise<Project> {
+    if (creatorId?.length === 0) throw ("creator_id should not be null");
+    if (projectId?.length === 0) throw ("project_id should not be null");
+
+    const userProjects = projectDB.filter(({ creator_id }) => creatorId === creator_id);
+    const projectData = userProjects.find(({id}) => id === projectId) as Project;
+
+    if(!projectData) throw("Project Not Found");
+
+    return projectData;
 }
