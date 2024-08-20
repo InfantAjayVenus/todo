afbaa384fe6dbd7588aba958d0ede7f38c1363fb
Infant Ajay Venus <infantcsajay@outlook.com>
2024-08-20 23:13:03 +0530

    [projects] patch: fetch creator ID from request in get Projects route

--- src/projects/routes.ts
+++ src/projects/routes.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { deleteProjectById, getProjects } from './controller';
+import { AuthenticatedRequest } from '../lib/middlewares/authMiddleware';
 
 export const projectRouter = express.Router();
 projectRouter.use(express.json());
 
-projectRouter.get('/', async (req, res) => {
+projectRouter.get('/', async (req: AuthenticatedRequest, res) => {
+    if(!req.creatorId) throw('Invalid Creator ID');
     try {
-        const filteredProjects = await getProjects('91823749');
+        const filteredProjects = await getProjects(req.creatorId);
         res.status(200).json(filteredProjects);
     } catch (error) {
         res.status(500).json({
