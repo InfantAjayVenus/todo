a6ead81a85236e1f135dab01e51d35231d9d7663
Infant Ajay Venus <infantcsajay@outlook.com>
2024-08-23 06:12:50 +0530

    [middleware] patch: add query to log in logger

--- src/lib/middlewares/logger.ts
+++ src/lib/middlewares/logger.ts
@@ -7,6 +7,7 @@ export const logger: RequestHandler = (req, _, next) => {
     console.log("URL:", req.url);
     console.log("METHOD:", req.method);
     req.params && console.log("REQUEST_PARAMETERS:", req.params);
+    req.query && console.log("REQUEST_PARAMETERS:", req.query);
     req.body && console.log("REQUEST_BODY:", req.body);
     next();
     console.log(`------------------------------------------------------------`);
