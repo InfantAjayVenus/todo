e4f03d26743164992db933e5ebabdce1a5ad9859
Infant Ajay Venus <infantcsajay@outlook.com>
2024-08-29 05:52:20 +0530

    [routes] refactor: restructure routing as routes and layout

--- src/Todo/index.tsx
+++ src/Todo/index.tsx
@@ -1,47 +1,26 @@
-import { AnimatePresence } from 'framer-motion';
-import { LuCalendar, LuCalendarDays, LuFolder, LuInbox } from 'react-icons/lu';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-import { Header, MainSection } from './components/MainLayout';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AddProject } from './routes/AddProject';
 import { Calendar } from './routes/Calendar';
 import { Projects } from './routes/Projects';
 import { TaskItem } from './routes/TaskItem';
 import { TasksList } from './routes/TasksList';
+import Layout from './routes/Layout';
 
 
 export default function Todo() {
 
     return (
         <BrowserRouter>
-            <Header>
-                <title>todo.</title>
-            </Header>
-            <MainSection>
-                <MainSection.Body>
-                    <AnimatePresence mode='wait'>
-                        <Routes>
-                            <Route index Component={TasksList} />
-                            <Route path='task/:taskId/' Component={TaskItem} />
-                            <Route path='calendar/' Component={Calendar} />
-                            <Route path='projects/'  Component={Projects} />
-                        </Routes>
-                    </AnimatePresence>
-                </MainSection.Body>
-
-                <MainSection.Navbar>
-                    <NavLink to='/?filter=today' >
-                        <MainSection.Navbar.NavbarItem icon={<LuCalendar className='text-4xl' />} label='Today' />
-                    </NavLink>
-                    <NavLink to='/calendar'>
-                        <MainSection.Navbar.NavbarItem icon={<LuCalendarDays className='text-4xl' />} label='Calendar' />
-                    </NavLink>
-                    <NavLink to='/?filter=inbox'>
-                        <MainSection.Navbar.NavbarItem icon={<LuInbox className='text-4xl' />} label='Inbox' />
-                    </NavLink>
-                    <NavLink to='/projects'>
-                        <MainSection.Navbar.NavbarItem icon={<LuFolder className='text-4xl' />} label='Projects' />
-                    </NavLink>
-                </MainSection.Navbar>
-            </MainSection>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route index element={<TasksList />} />
+                    <Route path=':taskId' element={<TaskItem />} />
+                    <Route path='calendar' element={<Calendar />} />
+                    <Route path='projects' element={<Projects />}>
+                        <Route path='new' element={<AddProject />} />
+                    </Route>
+                </Route>
+            </Routes>
         </BrowserRouter>
     );
 }
